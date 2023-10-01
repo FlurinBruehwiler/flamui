238f1102f10fc3f47625acd39c23c73a1c5503e6
Flurin Brühwiler <flurin.bruehwiler@cmiag.ch>
2023-10-01 15:37:58 +0200

    grrr it doesn't work

--- ImSharpUI.Sample/Program.cs
+++ ImSharpUI.Sample/Program.cs
@@ -1,6 +1,5 @@
 ﻿using SkiaSharp;
 using static SDL2.SDL;
-using static SkiaSharp.GRPixelConfig;
 
 if (SDL_Init(SDL_INIT_VIDEO) < 0)
 {
@@ -19,6 +18,9 @@ if (window == IntPtr.Zero)
     return;
 }
 
+var screenSurface = SDL_GetWindowSurface(window);
+
+
 // Create an OpenGL context
 var glContext = SDL_GL_CreateContext(window);
 if (glContext == IntPtr.Zero)
@@ -33,38 +35,33 @@ if (glContext == IntPtr.Zero)
 // Make the OpenGL context current
 SDL_GL_MakeCurrent(window, glContext);
 
-// Get the current OpenGL context
-var currentContext = SDL_GL_GetCurrentContext();
-if (currentContext != IntPtr.Zero)
-{
-    // Print some information about the context
-    Console.WriteLine($"Current OpenGL context: {currentContext}");
-}
-else
-{
-    // Handle error getting the context
-    Console.WriteLine("Failed to get the current OpenGL context");
-}
 
-var glInterface = GRGlInterface.AssembleGlInterface(currentContext, (contextHandle, name) => {
-    var addr = SDL_GL_GetProcAddress(name);
-    return addr;
-});
+var glInterface = GRGlInterface.CreateOpenGl(SDL_GL_GetProcAddress);
+Console.WriteLine(glInterface.Validate());
 
-var context = GRContext.Create(GRBackend.OpenGL, glInterface);
-var backendRenderTargetDescription = new GRBackendRenderTargetDesc
+// var context = GRContext.CreateGl(glInterface, new GRContextOptions
+// {
+//     AvoidStencilBuffers = true
+// });
+//
+// var target = new GRBackendRenderTarget(800, 600, 0, 8, new GRGlFramebufferInfo());
+//
+// var surface = SKSurface.Create(context, target, GRSurfaceOrigin.TopLeft, SKColorType.Rgba8888);
+
+GRContext context = GRContext.Create(GRBackend.OpenGL, glInterface);
+GRBackendRenderTargetDesc backendRenderTargetDescription = new GRBackendRenderTargetDesc
 {
-    Config = Rgba8888,
-    Height = 600,
-    Width = 800,
+    Config = GRPixelConfig.Rgba8888,
+    Height = 300,
+    Width = 300,
     Origin = GRSurfaceOrigin.TopLeft,
     RenderTargetHandle = new IntPtr(0),
     SampleCount = 0,
     StencilBits = 8
 };
-
 var surface = SKSurface.Create(context, backendRenderTargetDescription);
 var canvas = surface.Canvas;
+
 
 // Main loop
 var quit = false;
@@ -85,7 +82,8 @@ while (!quit)
     });
 
     // Swap the front and back buffers
-    SDL_GL_SwapWindow(window);
+    SDL_FillRect(screenSurface, IntPtr.Zero, 300);
+    SDL_UpdateWindowSurface(window);
 }
 
 surface.Dispose();
