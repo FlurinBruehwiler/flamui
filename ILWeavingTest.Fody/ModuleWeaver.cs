b5a0fd2bdf13e2fcc2950b11a7882934eee51d8b
Flurin Brühwiler <flurin.bruehwiler@cmiag.ch>
2025-06-19 21:06:42 +0200

    IL Weaving: temporary store of args in local variables

--- ILWeavingTest.Fody/ModuleWeaver.cs
+++ ILWeavingTest.Fody/ModuleWeaver.cs
@@ -1,4 +1,5 @@
-﻿using System.Collections.Generic;
+﻿using System;
+using System.Collections.Generic;
 using System.Linq;
 using Fody;
 using Mono.Cecil;
@@ -19,8 +20,15 @@ namespace ILWeavingTest.Fody
             {
                 foreach (var method in type.Methods)
                 {
-                    foreach (var parameter in method.Parameters)
+                    if (type.Name == "Ui" && method.IsStatic)
                     {
+                        uiMethods.Add(method);
+                        continue;
+                    }
+
+                    for (var i = 0; i < method.Parameters.Count; i++)
+                    {
+                        var parameter = method.Parameters[i];
                         if (parameter.ParameterType.Name == "Ui")
                         {
                             uiMethods.Add(method);
@@ -49,32 +57,68 @@ namespace ILWeavingTest.Fody
 
                     for (var i = 0; i < method.Body.Instructions.Count; i++)
                     {
-                        var ins = method.Body.Instructions[i];
+                        var targetCall = method.Body.Instructions[i];
 
-                        if (ins.OpCode == OpCodes.Call || ins.OpCode == OpCodes.Callvirt
-                            && ins.Operand is MethodReference calledMethod
+                        if (targetCall.OpCode == OpCodes.Call || targetCall.OpCode == OpCodes.Callvirt
+                            && targetCall.Operand is MethodReference calledMethod
                             && uiMethods.Contains(calledMethod.Resolve()))
                         {
                             if (il == null)
                                 il = method.Body.GetILProcessor();
 
+                            VariableDefinition uiVariableDefinition = null;
+                            VariableDefinition[] tempLocalVariables = new VariableDefinition[method.Parameters.Count]; //todo, can we get rid of this array alloc?
+
+                            //Store the original arguments as temp variables
+                            for (var p = 0; p < method.Parameters.Count; p++)
+                            {
+                                var parameter = method.Parameters[p];
+                                var tempLocal = new VariableDefinition(parameter.ParameterType); //todo can we get rid of this allocation?
+                                method.Body.Variables.Add(tempLocal);
+                                il.InsertBefore(targetCall, il.Create(OpCodes.Stloc, tempLocal));
+
+                                if (parameter.ParameterType.Name == "Ui")
+                                {
+                                    uiVariableDefinition = tempLocal;
+                                }
+                            }
+
+                            if (!method.IsStatic)
+                            {
+                                var tempLocal = new VariableDefinition(method.DeclaringType);
+                                method.Body.Variables.Add(tempLocal);
+                                il.InsertBefore(targetCall, il.Create(OpCodes.Stloc, tempLocal));
+
+                                if (method.DeclaringType.Name == "Ui")
+                                {
+                                    uiVariableDefinition = tempLocal;
+                                }
+                            }
+
+                            if (uiVariableDefinition == null)
+                                throw new Exception("Cound not find ui parameter");
+
                             //Load the ui variable onto the stack
-                            il.InsertBefore(ins,
-                                il.Create(OpCodes
-                                    .Ldarg_0)); //assuming the ui variable is the first parameter of the function
+                            il.InsertBefore(targetCall, il.Create(OpCodes.Ldloc, uiVariableDefinition));
 
                             //Load the hash onto the stack
-                            il.InsertBefore(ins, il.Create(OpCodes.Ldc_I4_S, ++uniqueNumbers));
+                            il.InsertBefore(targetCall, il.Create(OpCodes.Ldc_I4_S, ++uniqueNumbers));
 
                             //call the PushScope function
-                            il.InsertBefore(ins, il.Create(OpCodes.Callvirt, pushScopeMethod));
+                            il.InsertBefore(targetCall, il.Create(OpCodes.Callvirt, pushScopeMethod));
+
+                            //Restore the original arguments from temp variables
+                            for (var p = 0; p < tempLocalVariables.Length; p++)
+                            {
+                                var tempVariable = tempLocalVariables[p];
+                                il.InsertBefore(targetCall, il.Create(OpCodes.Ldloc, tempVariable));
+                            }
 
                             //call the actual method
 
                             //Load the ui variable onto the stack
                             var loadIns = il.Create(OpCodes.Ldarg_0);
-                            il.InsertAfter(ins,
-                                loadIns); //assuming the ui variable is the first parameter of the function
+                            il.InsertAfter(targetCall, loadIns); //assuming the ui variable is the first parameter of the function
 
                             //call the PopScope function
                             il.InsertAfter(loadIns, il.Create(OpCodes.Callvirt, popScopeMethod));
