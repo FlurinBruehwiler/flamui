ffe3f6f17b7ac740421516d15c7dea2d96b3e59f
Flurin Brühwiler <flurin.bruehwiler@cmiag.ch>
2025-09-09 15:28:55 +0200

    Update FlexSizeCalculator.cs

--- Flamui/UiElements/FlexSizeCalculator.cs
+++ Flamui/UiElements/FlexSizeCalculator.cs
@@ -39,7 +39,11 @@ public static class FlexSizeCalculator
 
             totalFixedSize += child.UiElementInfo.Margin.SumInDirection(info.Direction);
 
-            if (child.IsFlexible(out var config))
+            //first layout fixed children (main axis constraint should not matter!)
+            //then layout dynamic children (main axis constraint should be more accurate)
+            //then layout percentage based children (main axis constraint is tight)
+
+            if (child.FlexibleChildConfig.SizeKind == ChildSizeKind.Percentage)
             {
                 if (isMainShrink)
                 {
@@ -55,42 +59,65 @@ public static class FlexSizeCalculator
                     // }
                 }
 
-                totalPercentage += config.Percentage;
+                totalPercentage += child.FlexibleChildConfig.Percentage;
                 continue;
             }
 
-            var maxMain = children.Count == 1 ? constraint.GetMainAxis(info.Direction).Max - info.PaddingSizeMain() : float.PositiveInfinity; //todo, this is false, should only count if the count of flexible children == 1
-            var childConstraint = BoxConstraint.FromDirection(info.Direction, 0, maxMain, 0,
-                constraint.GetCrossAxis(info.Direction).Max - info.PaddingSizeCross() -
-                child.UiElementInfo.Margin.SumInDirection(info.Direction.Other()));
-            var size = child.Layout(childConstraint);
+            if (child.FlexibleChildConfig.SizeKind == ChildSizeKind.Fixed)
+            {
+                var maxMain = float.PositiveInfinity;//it shouldn't really matter what we pick here, because the child size should be fixed...
+                var childConstraint = BoxConstraint.FromDirection(info.Direction, 0, maxMain, 0,
+                    constraint.GetCrossAxis(info.Direction).Max - info.PaddingSizeCross() -
+                    child.UiElementInfo.Margin.SumInDirection(info.Direction.Other()));
+                var size = child.Layout(childConstraint);
 
-            totalFixedSize += size.GetMainAxis(info.Direction);
+                totalFixedSize += size.GetMainAxis(info.Direction);
 
-            maxCrossSize = Math.Max(maxCrossSize, size.GetCrossAxis(info.Direction) + child.UiElementInfo.Margin.SumInDirection(info.Direction.Other()));
+                maxCrossSize = Math.Max(maxCrossSize, size.GetCrossAxis(info.Direction) + child.UiElementInfo.Margin.SumInDirection(info.Direction.Other()));
+            }
         }
 
         totalFixedSize += info.PaddingSizeMain() + TotalGapSize(relevantChildCount, info);
 
         var availableSize = constraint.GetMainAxis(info.Direction).Max - totalFixedSize;
+
+        foreach (var child in children)
+        {
+            if (child.FlexibleChildConfig.SizeKind == ChildSizeKind.Dynamic)
+            {
+                var maxMain = availableSize; //todo this doesn't work correct right now, it seems to be a bit too big right now...
+                var childConstraint = BoxConstraint.FromDirection(info.Direction, 0, maxMain, 0,
+                    constraint.GetCrossAxis(info.Direction).Max - info.PaddingSizeCross() -
+                    child.UiElementInfo.Margin.SumInDirection(info.Direction.Other()));
+                var size = child.Layout(childConstraint);
+
+                totalFixedSize += size.GetMainAxis(info.Direction);
+
+                maxCrossSize = Math.Max(maxCrossSize, size.GetCrossAxis(info.Direction) + child.UiElementInfo.Margin.SumInDirection(info.Direction.Other()));
+            }
+        }
+
+        //recalculate available size
+        availableSize = constraint.GetMainAxis(info.Direction).Max - totalFixedSize;
+
         var sizePerPercentage = GetSizePerPercentage(totalPercentage, availableSize);
 
         //layout all flexible children
         foreach (var child in children)
         {
-            if (!child.IsFlexible(out var config))
-                continue;
+            if (child.FlexibleChildConfig.SizeKind == ChildSizeKind.Percentage)
+            {
+                if (child.UiElementInfo.AbsoluteInfo.HasValue)
+                    continue;
 
-            if(child.UiElementInfo.AbsoluteInfo.HasValue)
-                continue;
+                var mainSizeConstraint = child.FlexibleChildConfig.Percentage * sizePerPercentage;
 
-            var mainSizeConstraint = config.Percentage * sizePerPercentage;
+                var size = child.Layout(BoxConstraint.FromDirection(info.Direction, mainSizeConstraint, mainSizeConstraint, 0,
+                    constraint.GetCrossAxis(info.Direction).Max - info.PaddingSizeCross() - child.UiElementInfo.Margin.SumInDirection(info.Direction.Other())));
 
-            var size = child.Layout(BoxConstraint.FromDirection(info.Direction, mainSizeConstraint, mainSizeConstraint, 0,
-                constraint.GetCrossAxis(info.Direction).Max - info.PaddingSizeCross() - child.UiElementInfo.Margin.SumInDirection(info.Direction.Other())));
-
-            totalFixedSize += size.GetMainAxis(info.Direction);
-            maxCrossSize = Math.Max(maxCrossSize, size.GetCrossAxis(info.Direction));
+                totalFixedSize += size.GetMainAxis(info.Direction);
+                maxCrossSize = Math.Max(maxCrossSize, size.GetCrossAxis(info.Direction));
+            }
         }
 
         return BoxSize.FromDirection(info.Direction, totalFixedSize, maxCrossSize + info.PaddingSizeCross());
